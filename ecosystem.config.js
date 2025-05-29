d30f79d0614662e5012559e778d4ee85a5cff70c
R3v3ill3 <troy@reveille.net.au>
2025-05-29 16:40:18 +1000

    Fix permissions and PM2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,16 @@ module.exports = {
     name: 'union-campaign-planner',
     script: 'npm',
     args: 'run dev',
-    cwd: './home/project',
+    cwd: '/home/project',
     watch: false,
     env: {
       NODE_ENV: 'development',
       PORT: 5250,
       HOST: true
     },
-    out_file: './logs/out.log',
-    error_file: './logs/error.log',
-    log_file: './logs/combined.log',
+    out_file: '/home/project/logs/out.log',
+    error_file: '/home/project/logs/error.log',
+    log_file: '/home/project/logs/combined.log',
     time: true,
     combine_logs: true,
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z'

08f5c9399b65dfd29257719bb04a53ed39e6bf35
R3v3ill3 <troy@reveille.net.au>
2025-05-29 16:49:23 +1000

    Update PM2 configuration with correct paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
   apps: [{
     name: 'union-campaign-planner',
-    script: 'npm',
+    script: '/usr/bin/npm',
     args: 'run dev',
     cwd: '/var/www/campaign-planner/union-gem',
     watch: false,
     env: {
       NODE_ENV: 'development',
       PORT: 5250,
-      HOST: true
+      HOST: true,
+      PATH: '/usr/local/bin:/usr/bin:/bin'
     },
     out_file: '/var/www/campaign-planner/union-gem/logs/out.log',
     error_file: '/var/www/campaign-planner/union-gem/logs/error.log',
